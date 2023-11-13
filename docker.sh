7e1db2246baee7600ab951a08fac8b1458d5458a
Ubuntu <ubuntu@ip-172-31-44-9.ap-south-1.compute.internal>
2023-11-13 06:28:15 +0000

    Added Docker Script

--- docker.sh
+++ docker.sh
@@ -1,2 +1,32 @@
-this is docker file
+#!/bin/bash
+
+# Update the package index
+sudo apt update
+
+# Install packages to allow apt to use a repository over HTTPS
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+# Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Set up the stable Docker repository
+echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the package index again
+sudo apt update
+
+# Install the latest version of Docker Engine and containerd
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Add your user to the 'docker' group to run Docker without sudo
+sudo usermod -aG docker $USER
+
+# Restart the system to apply group changes
+sudo systemctl restart docker
+
+echo "Docker Engine has been successfully installed. Please log out and log back in to apply group changes."
 
